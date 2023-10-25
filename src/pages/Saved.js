bf42b1853c3115f5ed70b0aa0881e64cbea89327
Bhoomi S <bhoomi.s@cementdigital.com>
2023-10-25 15:36:48 +0530

    json changes

--- src/pages/Saved.js
+++ src/pages/Saved.js
@@ -1,9 +1,48 @@
-import React from 'react'
+
+
+import React, { useEffect , useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function Saved() {
+
+  const navigate = useNavigate();
+  const [json, setJson] = useState({
+    name: 'bhoomi',
+    age: 42,
+    address: {
+      street: '123 Main St.',
+      city: 'Boston',
+      state: 'MA'
+    }
+  });
+
+  // This function updates the "json" state by changing the "name" property
+  const updateName = () => {
+    setJson({
+      ...json, // copy the current properties of "json"
+      name: 'patel' // update the "name" property
+    });
+  };
+  useEffect(() => {
+
+    const token = localStorage.getItem('accessToken')
+    if (!token) {
+      navigate('/login');
+    }
+  }, []);
+
+
   return (
+    <>
     <div className="title">
-      Saved
+      Messages
     </div>
+     <div>
+     <p>Name: {json.name}</p>
+     <button onClick={updateName}>Update Name</button>
+   </div>
+   </>
   )
 }
+
+
